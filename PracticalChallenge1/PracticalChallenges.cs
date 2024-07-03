f11a641572f6da440d5592f68a6800362a605f30
Lucas <lucas.andrade@e-gestao.com.br>
2024-07-03 18:06:39 -0300

    Add project files.

---
+++ PracticalChallenge1/PracticalChallenges.cs
@@ -0,0 +1,45 @@
+﻿using PracticalChallenges.Programs;
+
+namespace PracticalChallenges;
+
+internal class PracticalChallenges ()
+{
+      static void WelcomeMenu()
+    {
+        Challenge1 program = new Challenge1();
+
+        Console.WriteLine("\nSelect which program you want to use:\n" +
+            "1. Welcome Message\n" +
+            "2. Name Concat\n" +
+            "3. Math Operations\n" +
+            "4. Number of characters\n" +
+            "5. Valid car plate\n" +
+            "6. Format date\n");
+
+        var answer = Console.ReadLine() ?? "";
+
+        switch (answer)
+        {
+            case "1":
+                program.WelcomeMessage();
+                break;
+            case "2":
+                program.NameConcat();
+                break;
+            case "3":
+                program.MathOperations();
+                break;
+            default:
+                break;
+        }
+        WelcomeMenu();
+
+    }
+      static void Main()
+    {
+
+        WelcomeMenu();
+       
+    }
+
+}
---
+++ PracticalChallenge1/Programs/Challenge1.cs
@@ -0,0 +1,61 @@
+﻿namespace PracticalChallenges.Programs;
+
+public class Challenge1
+{
+    public void WelcomeMessage()
+    {
+        Console.WriteLine("Type your name out:");
+
+        var name = Console.ReadLine();
+
+        Console.WriteLine($"Welcome, {name}. We're glad you're here.");
+    }
+
+    public void NameConcat()
+    {
+        Console.WriteLine("Type your name out:");
+
+        var name = Console.ReadLine();
+
+        Console.WriteLine("Now your surname:");
+
+        var surname = Console.ReadLine();
+
+        Console.WriteLine($"{name} {surname}");
+    }
+    public void MathOperations()
+    {
+        double x;
+        double y;
+
+        Console.WriteLine("Type the first number:");
+        Double.TryParse(Console.ReadLine(), out x);
+
+        Console.WriteLine("Type the second number:");
+        Double.TryParse(Console.ReadLine(), out y);
+
+        double sum = x + y;
+        double subtraction = x - y;
+        double multiplication = x * y;
+        double division = x / y;
+        double mean = (x + y) / 2;
+
+        Console.WriteLine($"Sum is {sum}\n" +
+            $"Subtraction is {subtraction}\n" +
+            $"Multiplication is {multiplication}\n" +
+            $"Divsion is {division}\n" +
+            $"Mean is {mean}");
+    }
+
+    public void CheckNumberOfCharacters()
+    {
+        Console.WriteLine("Type one or more words:");
+        var words = Console.ReadLine();
+
+        words.ToList().ForEach
+    }
+    public void List()
+    {
+        List<int> integers = new List<int>();
+    }
+}
